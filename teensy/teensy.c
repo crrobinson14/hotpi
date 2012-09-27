554c4b078c360fc58635eb53c379eca93b0f5480
Chad Robinson <crobinson@medialantern.com>
2012-09-27 11:09:10 -0700

    Basic Teensy project

---
+++ teensy/teensy.c
@@ -0,0 +1,17 @@
+/*
+ * teensy.c
+ *
+ * Created: 9/27/2012 11:06:40 AM
+ *  Author: Administrator
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
