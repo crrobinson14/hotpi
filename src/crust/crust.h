acfd6931e30c79e40087b3efcfbee367a82457b8
Chad Robinson <taskswap@gmail.com>
2012-12-31 11:32:40 -0500

    Begin fleshing out crust service.

---
+++ src/crust/crust.h
@@ -0,0 +1,17 @@
+#ifndef CRUST_H_
+#define CRUST_H_
+
+#define F_CPU 16000000UL
+
+#include <avr/io.h>
+#include <avr/pgmspace.h>
+#include <avr/power.h>
+#include <util/delay.h>
+#include <util/twi.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <avr/sfr_defs.h>
+
+#include "serial.h"
+
+#endif
---
+++ src/crust/serial.h
@@ -0,0 +1,13 @@
+#ifndef SERIAL_H_
+#define SERIAL_H_
+
+extern void serial_init(void);
+extern void serial_send(void);
+extern void serial_read(void);
+
+extern uint8_t serial_buffer_out[32];
+
+extern uint8_t serial_bytes_in;
+extern uint8_t serial_buffer_in[32];
+
+#endif
